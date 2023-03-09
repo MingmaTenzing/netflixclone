60915117547a1e6be5afd68dbf8034f048ac4a5f
MingmaTenzing <pasangsherpa689@gmail.com>
2023-03-09 08:57:26 +0800

    made changes to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,14 +3,14 @@ import Netflixlogo from "../assests/Netflixlogo.svg"
 
 function Nav() {
   return (
-    <main className="flex justify-between">
+    <main className="flex justify-between  lg:p-4">
         <div>
-            <Image src={Netflixlogo} width={200} height={200} className='w-20' alt='Netflix Logo' />
+            <Image src={Netflixlogo} width={200} height={200} className='w-20 sm:w-[120px] lg:w-[150px]' alt='Netflix Logo' />
 
         </div>
 
         <div className="">
-            <button className=" py-1 px-2 rounded-lg bg-NetflixRed  text-white text-sm"> Sign in</button>
+            <button className=" py-1 px-2 lg:px-3 rounded-lg bg-NetflixRed  text-white text-sm  sm:text-base md:text-lg"> Sign in</button>
 
         </div>
         
